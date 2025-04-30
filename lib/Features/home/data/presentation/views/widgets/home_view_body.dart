80e2d42fbccf3e90e1af53a501c098459dc36e5f
Hussein Rizk <husseinrizk117@gmail.com>
2025-04-30 20:07:51 +0300

    best saller item part

--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -19,8 +21,8 @@ class HomeViewBody extends StatelessWidget {
             CustomAppBar(),
             FeaturedBooksListView(),
             SizedBox(height: 50),
-            Text('Best Sellar', style: Styles.titleMedium),
-            SizedBox(height: 50),
+            Text('Best Sellar', style: Styles.text18),
+            SizedBox(height: 20),
 
             BestSellerListViewItem(),
           ],
@@ -50,6 +52,20 @@ class BestSellerListViewItem extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          SizedBox(width: 30),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.text20,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+            ],
           ),
         ],
       ),
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-  );
+  static const text18 = TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+  static const text20 = TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
+  static const text30 = TextStyle(fontSize: 30, fontWeight: FontWeight.normal);
+  static const text14 = TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+  static const text16 = TextStyle(fontSize: 16, fontWeight: FontWeight.normal);
 }
