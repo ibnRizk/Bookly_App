27b6e0ac5540abed8e3bfe6ad1b5448c0ecb1d11
Hussein Rizk <husseinrizk117@gmail.com>
2025-05-02 01:03:24 +0300

    create custom book details appbar

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'custom_book_details_appbar.dart';
+
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(children: [CustomBookDetailAppbar()]),
+    );
   }
 }
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,16 +1,20 @@
-import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../Features/home/presentation/views/book_details_view.dart';
+import '../../Features/home/presentation/views/home_view.dart';
 import '../../Features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/BookDetailsView';
+
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => SplashView()),
+      GoRoute(path: kHomeView, builder: (context, state) => HomeView()),
       GoRoute(
-        path: AppRouter.kHomeView,
-        builder: (context, state) => HomeView(),
+        path: kBookDetailsView,
+        builder: (context, state) => BookDetailsView(),
       ),
     ],
   );
