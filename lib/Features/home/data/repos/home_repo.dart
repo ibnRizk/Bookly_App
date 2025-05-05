fa6732cd354139b68720b3d37d2b4179e0d91f1f
Hussein Rizk <husseinrizk117@gmail.com>
2025-05-05 03:56:01 +0300

    featch similer books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -6,4 +6,7 @@ import '../models/book_model/book_model.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({
+    required String category,
+  });
 }
